fe963d31864aae82b33cf379ffa3b03325ee1856
Nicky Semenza <14nicholasse@gmail.com>
2016-02-10 02:09:29 -0500

    some click to scroll + media queries

--- script.js
+++ script.js
@@ -18,12 +18,12 @@
                 templateUrl : 'pages/resume.html',
                 controller  : 'mainController'
             })
-            .otherwise({ redirectTo: '/404' });
+            .otherwise({ redirectTo: '/' });
     });
    app.controller('keyController', function($scope, $rootScope) {
        $rootScope.bodyClass="pathway-body";
    });
-    app.controller('mainController', function($scope, $rootScope) {
+    app.controller('mainController', function($scope, $rootScope,anchorSmoothScroll, $location) {
         $rootScope.bodyClass="stars-body";
         $scope.pageTitle = "Nicky Semenza";
         $scope.projects = [
@@ -136,8 +136,72 @@ $scope.bricks=[];
             }
         }
         pic();
+
+
+        $scope.gotoElement = function (eID){
+          // set the location.hash to the id of
+          // the element you wish to scroll to.
+          $location.hash('about');
+     
+          // call $anchorScroll()
+          anchorSmoothScroll.scrollTo(eID);
+      
+        };
         
     });
+  app.service('anchorSmoothScroll', function(){
+    
+    this.scrollTo = function(eID) {
+
+        // This scrolling function 
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+        
+        var startY = currentYPosition();
+        var stopY = elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+        
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+        
+        function elmYPosition(eID) {
+            var elm = document.getElementById(eID);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+
+    };
+    
+});
 
 
 
