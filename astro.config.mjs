2b44d70e585e6acf48fc92c7a15a2804ff725b7b
Nicky Semenza <nicky@nickysemenza.com>
2025-10-12 10:11:57 -0700

    add compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,12 @@ import rehypeExternalLinks from "rehype-external-links";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nickysemenza.com",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [mdx(), sitemap(), icon(), playformCompress()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
